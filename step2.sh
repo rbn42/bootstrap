be279b9f60f037a94d8a51b6f65b340480637378
rbn42 <bl100@students.waikato.ac.nz>
2016-05-08 04:00:29 +1200

    step2

---
+++ step2.sh
@@ -0,0 +1,12 @@
+ln -s /usr/share/zoneinfo/Pacific/Auckland /etc/localtime
+mkinitcpio -p linux
+echo en_US.UTF-8 UTF-8   >> /etc/locale.gen 
+echo zh_CN.UTF-8 UTF-8   >> /etc/locale.gen 
+locale-gen
+
+pacman -S  grub
+pacman -S dialog
+grub-install  /dev/sda
+grub-mkconfig -o /boot/grub/grub.cfg
+
+pacman -S sudo
