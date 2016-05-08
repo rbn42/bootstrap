0b3972e6ba15aa02507365f117fe33d587f0026c
rbn42 <bl100@students.waikato.ac.nz>
2016-05-08 23:41:20 +1200

    step

--- install_ubuntu_chroot.sh
+++ install_ubuntu_chroot.sh
@@ -1,29 +1,10 @@
 #安装工具
 sudo apt-get install debootstrap
-#以下全程root操作
-sudo su
 #格式化一个分区,挂载到/mnt/installer
 gparted
-export TARGET_DISK="/dev/sda6"
-mkdir /mnt/installer
-mount $TARGET_DISK /mnt/installer
-#下载ubuntu 14.04 系统
-debootstrap trusty /mnt/installer http://archive.ubuntu.com/ubuntu
-#chroot到14.04系统
-mount --bind /dev /mnt/installer/dev
-mount --bind /dev/pts /mnt/installer/dev/pts
-mount -t proc proc /mnt/installer/proc
-mount -t sysfs sys /mnt/installer/sys
-
-cd /mnt/installer/root
-git clone https://github.com/rbn42/chroot_script.git
-git checkout ubuntu1404
-
-#默认的sources.list里面的源很少，我这里宿主系统就是ubuntu，所以直接复制进去，然后再修改源的版本
-#否则的话上网找一份吧
-cat  /etc/sources.list > /mnt/install/etc/apt/sources.list
-vi /mnt/install/etc/apt/sources.list
-
+cd ~/git/chroot_script
+sudo su
+bash step1.sh
 chroot /mnt/installer /bin/bash
 cd ~/chroot_script
 bash step2.sh
---
+++ step1.sh
@@ -0,0 +1,21 @@
+#以下全程root操作
+export TARGET_DISK="/dev/sda6"
+mkdir /mnt/installer
+mount $TARGET_DISK /mnt/installer
+#下载ubuntu 14.04 系统
+debootstrap trusty /mnt/installer http://archive.ubuntu.com/ubuntu
+#chroot到14.04系统
+mount --bind /dev /mnt/installer/dev
+mount --bind /dev/pts /mnt/installer/dev/pts
+mount -t proc proc /mnt/installer/proc
+mount -t sysfs sys /mnt/installer/sys
+
+
+#默认的sources.list里面的源很少，我这里宿主系统就是ubuntu，所以直接复制进去，然后再修改源的版本
+#否则的话上网找一份吧
+cat sources.list > /mnt/installer/etc/apt/sources.list
+
+cd /mnt/installer/root
+git clone https://github.com/rbn42/chroot_script.git
+cd chroot_script
+git checkout ubuntu1404
