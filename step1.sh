e24a460074b042ca13b227bc05d1dbcb01eb7732
rbn42 <bl100@students.waikato.ac.nz>
2016-05-09 00:30:53 +1200

    steps

--- step1.sh
+++ step1.sh
@@ -7,8 +7,8 @@ debootstrap trusty /mnt/installer http://archive.ubuntu.com/ubuntu
 #chroot到14.04系统
 mount --bind /dev /mnt/installer/dev
 mount --bind /dev/pts /mnt/installer/dev/pts
-mount -t proc proc /mnt/installer/proc
-mount -t sysfs sys /mnt/installer/sys
+mount -t proc /proc /mnt/installer/proc
+mount -t sysfs /sys /mnt/installer/sys
 
 
 #默认的sources.list里面的源很少，我这里宿主系统就是ubuntu，所以直接复制进去，然后再修改源的版本
