441614bd1495f605089d25c784dd7ca12601e327
rbn42 <bl100@students.waikato.ac.nz>
2016-05-09 02:29:13 +1200

    step

--- step2.sh
+++ step2.sh
@@ -48,15 +48,4 @@ apt update -y
 apt upgrade -y
 apt-get install  ubuntu-standard -y
 #安装ubuntu unity桌面，这个超大，1.6G
-apt-get install ubuntu-desktop -y
-#我自己用的常用软件。
-apt install trash-cli  -y
-apt -y install vim-gnome git  okular indicator-multiload
-apt -y install gparted cmake xbacklight wine comix vlc
-apt -y install aria2 goldendict python-autopep8
-apt-get install -y unattended-upgrades
-apt-get install aegisub -y
-apt install ncmpcpp mpd -y
-#删除一些讨厌的东西
-rm /usr/share/applications/webbrowser-app.desktop 
-sudo apt remove apport 
+apt-get install ubuntu-desktop -y 
---
+++ step3.sh
@@ -0,0 +1,11 @@
+#我自己用的常用软件。
+apt install trash-cli  -y
+apt -y install vim-gnome git  okular indicator-multiload
+apt -y install gparted cmake xbacklight wine comix vlc
+apt -y install aria2 goldendict python-autopep8
+apt-get install -y unattended-upgrades
+apt-get install aegisub -y
+apt install ncmpcpp mpd -y
+#删除一些讨厌的东西
+rm /usr/share/applications/webbrowser-app.desktop 
+sudo apt remove apport 
