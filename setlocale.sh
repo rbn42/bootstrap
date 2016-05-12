d9feacbe29413f4ec443002be7f2c688f90b4e8a
rbn42 <bl100@students.waikato.ac.nz>
2016-05-13 01:41:43 +1200

    gentoo

--- setlocale.sh
+++ setlocale.sh
@@ -1,11 +1,4 @@
 ######################################
-#以下操作在chroot系统中进行
-#设置语言，自行调整
-#echo LANG=$1_$2.$3      >   /etc/default/locale 
-#echo LANGUAGE=$1_$2:$1  >>  /etc/default/locale
-#echo LC_ALL=$1_$2.$3    >>  /etc/default/locale
-export LC_ALL=$1_$2.$3  
-#locale-gen 
 echo en_US.UTF-8 UTF-8 >> /etc/locale.gen
 echo en_NZ.UTF-8 UTF-8 >> /etc/locale.gen
 echo zh_CN.UTF-8 UTF-8 >> /etc/locale.gen
