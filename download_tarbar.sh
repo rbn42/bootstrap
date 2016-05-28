6f8d06d2afc5e64ce37a6ce1f2694c26b2419240
rbn42 <bl100@students.waikato.ac.nz>
2016-05-29 11:12:16 +1200

    failed

--- download_tarbar.sh
+++ download_tarbar.sh
@@ -1,5 +1,5 @@
-cd /mnt/funtoo
-wget "http://distfiles.gentoo.org/releases/amd64/autobuilds/20160505/stage3-amd64-20160505.tar.bz2"
-tar xvjpf stage3-*.tar.bz2 --xattrs
+cd /mnt/gentoo
+wget $1
+tar xvjpf *.tar.bz2 --xattrs
 wget "http://distfiles.gentoo.org/snapshots/portage-latest.tar.bz2"
 tar xfpj portage-latest.tar.bz2 -C /mnt/sabayon/usr
--- install_gentoo.sh
+++ install_gentoo.sh
@@ -1,20 +1,24 @@
+#在ubuntu1604环境下安装
+#stage4的编译倒是没什么问题
+#失败于equo rescue generate这个步骤,过不去
 sudo su
-export DEV_ROOT=/dev/sda7
+export DEV_ROOT=/dev/sda2
 export DEV_HOME=/dev/sda5
+export TARBAR="http://distfiles.gentoo.org/releases/amd64/autobuilds/20160526/stage4-amd64-minimal-20160526.tar.bz2"
 #格式化一个分区,挂载到/mnt/installer
 bash step1.sh $DEV_ROOT
 ln -s /mnt/installer /mnt/sabayon
 ln -s /mnt/installer /mnt/gentoo
-bash ./download_tarbar.sh
+bash ./download_tarbar.sh $TARBAR
 #生成fstab，默認關閉了/home
 bash ./gen-fstab.sh $DEV_ROOT $DEV_HOME > /mnt/installer/etc/fstab
 #mkdir /mnt/gentoo/etc/portage/repos.conf
 #cp /mnt/gentoo/usr/share/portage/config/repos.conf /mnt/gentoo/etc/portage/repos.conf/gentoo.conf
 
-rm /dev/shm && mkdir /dev/shm
-mount -t tmpfs -o nosuid,nodev,noexec shm /dev/shm
-#Also ensure that mode 1777 is set
-chmod 1777 /dev/shm
+#rm /dev/shm && mkdir /dev/shm
+#mount -t tmpfs -o nosuid,nodev,noexec shm /dev/shm
+##Also ensure that mode 1777 is set
+#chmod 1777 /dev/shm
 
 bash ./chroot.sh
 cd
@@ -26,17 +30,19 @@ env-update
 eselect python set 1    #sets python 2.7 as default
 source /etc/profile
 ls -sf /usr/share/zoneinfo/Pacific/Auckland /etc/localtime 
-echo hostname=\"sabayon\" > /etc/conf.d/hostname
-echo 127.0.0.1 sabayon > /etc/hosts
+#echo hostname=\"sabayon\" > /etc/conf.d/hostname
+#echo 127.0.0.1 sabayon > /etc/hosts
 
 #
+mv /usr/portage/metadata/timestamp.x  ./
 emerge-webrsync
 emerge --sync
-emerge -auDN @world
+#emerge -auDN @world
 #Entropy and Equo
 emerge -avt equo --autounmask-write
 etc-update
 emerge -avt equo
+emerge -avt entropy
 
 #First things first, knowing that Entropy databases are sqlite3, I created an empty sqlite3 database :
 
