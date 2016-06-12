a854fee36d1af58864b82267d55397a24c386e7c
rbn42 <bl100@students.waikato.ac.nz>
2016-06-12 21:15:30 +1200

    m

--- extra.sh
+++ extra.sh
@@ -5,10 +5,15 @@ sudo ln -s /run/media
 #nvidia-xconfig
 
 #compiz installation is complex
+#compiz编译在这台电脑上很容易失败,所以只能开机后进入tty单开yaourt编译.
 #yaourt -S compiz
 #yaourt -S emerald0.9
 
 #sudo pacman -S cuda 
 #yaourt -S python-theano
+
+#这个是1.9版本的,但是我们需要的是1.10版本.panda3d网上只提供了deb包,所以我们要自己编译了.
+#安装下面的包或许可以帮忙把依赖装上
+#yaourt -S panda3d
 
 ###yaourt -S emerald-themes
