4183431fe5b38c6f5b951a975752807ea21d45a8
rbn42 <bl100@students.waikato.ac.nz>
2016-06-12 18:20:44 +1200

    pacstrap

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
+#ubuntu下安装没有问题,arch下尝试安装会导致宿主机崩溃
 sudo su
-bash ./download_bootstrap.sh "https://mirrors.kernel.org/archlinux/iso/2016.05.01/archlinux-bootstrap-2016.05.01-x86_64.tar.gz"
+bash ./download_bootstrap.sh "https://mirrors.kernel.org/archlinux/iso/2016.06.01/archlinux-bootstrap-2016.06.01-x86_64.tar.gz"
 #chroot 结束后,尝试删除/tmp中的残余文件,结果导致运行系统崩溃,不过还好似乎没有触及硬盘系统的文件.
 bash ./chroot_bootstrap.sh
 
 cd
-bash ./chroot_installer.sh /dev/sda8
+#导致宿主arch崩溃的步骤 
+bash ./chroot_installer.sh /dev/sda6
 arch-chroot /mnt
 
 cd
