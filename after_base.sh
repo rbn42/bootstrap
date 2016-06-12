3a46cbfd9806f17b9eb781cade701bb1d5e1d58b
rbn42 <bl100@students.waikato.ac.nz>
2016-06-12 18:49:36 +1200

    pack

--- after_base.sh
+++ after_base.sh
@@ -1,16 +1,25 @@
-pacman -S nvidia
+pacman -S nvidia xorg xfce4 \
+    conky \
+    adobe-source-code-pro-fonts adobe-source-han-sans-cn-fonts \
+    adobe-source-han-sans-tw-fonts adobe-source-sans-pro-fonts \
+    adobe-source-serif-pro-fonts \
+    ttf-arphic-ukai ttf-arphic-uming ttf-dejavu ttf-droid ttf-freefont \
+    ttf-linux-libertine ttf-ubuntu-font-family wqy-microhei wqy-microhei-lite \
+    wqy-zenhei \
+    asciidoc autoconf automake autopep8 cmake conky ctags dialog fcitx-configtool \
+    fcitx-gtk2 fcitx-gtk3 fcitx-qt4 fcitx-qt5 fcitx-rime fcitx-ui-light feh \
+    fish gcc git glibc gparted gvim gzip imagemagick ipython ipython2 make ntfs-3g \
+    opendesktop-fonts pamixer pkg-config pulseaudio pulseaudio-alsa python-docopt \
+    python-nose python-numpy python-pandas python-pip python-scipy python2-numpy \
+    python2-pip python2-scipy qt5-graphicaleffects qt5-quickcontrols screenfetch \
+    scrot trash-cli w3m wget wmctrl xdotool \
+    firefox \
+    thunar \
+    thunderbird \
+
 #nvidia-xconfig
-pacman -S xorg
-pacman -S xfce4
 #startx
 
 #pacman -S xbindkeys
 #xbindkeys
-
-pacman -S conky 
-
-sudo pacman -S adobe-source-code-pro-fonts adobe-source-han-sans-cn-fonts adobe-source-han-sans-tw-fonts adobe-source-sans-pro-fonts adobe-source-serif-pro-fonts 
-sudo pacman -S ttf-arphic-ukai ttf-arphic-uming ttf-dejavu ttf-droid ttf-freefont ttf-linux-libertine ttf-ubuntu-font-family wqy-microhei wqy-microhei-lite wqy-zenhei 
-
-sudo pacman -S asciidoc autoconf automake autopep8 cmake conky ctags dialog fcitx-configtool fcitx-gtk2 fcitx-gtk3 fcitx-qt4 fcitx-qt5 fcitx-rime fcitx-ui-light feh firefox fish gcc git glibc gparted gvim gzip imagemagick ipython ipython2 make ntfs-3g opendesktop-fonts pamixer pkg-config pulseaudio pulseaudio-alsa python-docopt python-nose python-numpy python-pandas python-pip python-scipy python2-numpy python2-pip python2-scipy qt5-graphicaleffects qt5-quickcontrols screenfetch scrot thunar thunderbird trash-cli w3m wget wmctrl xdotool 
 #ln -s /opt/cuda/  /usr/local/cuda-7.5       
