eaf5e08d49b23f7f9f5063a887314cb6439bd877
rbn42 <bl100@students.waikato.ac.nz>
2016-08-18 02:55:17 +1200

    rebase to 0.8.12.3

--- old/compiz.sh
+++ old/compiz.sh
@@ -1,20 +1,18 @@
+#compiz-core-git的版本,不过不稳定.
 mkdir ~/arch-build
-rm ~/arch-build/compiz-core -rf
+rm ~/arch-build/compiz-core-git -rf
 cd ~/arch-build
 
 cd ~/arch-build
 #需要是git版本的,不然做不了git操作
-git clone https://aur.archlinux.org/compiz-core.git
-cd ~/arch-build/compiz-core
+git clone https://aur.archlinux.org/compiz-core-git.git
+cd ~/arch-build/compiz-core-git
 
 #添加这部分到PKGBUILD尾部
-#因为知道这个版本是0.8.12.3所以才这么写,下个版本需要改动.
 echo """
 prepare() {
-  cd \"\${srcdir}/\${_upstream}-\${pkgver}\"
-  git clone https://github.com/compiz-reloaded/compiz.git;
-  cd compiz;git checkout v0.8.12.3;
-  mv .git ../;cd ..;
+  cd \"\${srcdir}/\${_upstream}\";
+  git checkout v0.8.12.3
   git remote add repo https://github.com/rbn42/compiz.git;
   git pull repo mystable --no-edit;
   #git merge repo/increase-blur-radius --no-edit;
