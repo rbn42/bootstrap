e1def7ba6d074f73aeb7d6428602511bcdc761a8
rbn42 <bl100@students.waikato.ac.nz>
2016-10-31 12:48:19 +1300

    groups.sh

--- groups.sh
+++ groups.sh
@@ -2,11 +2,14 @@
 #有用的不多
 #大部分是DE的group
 sudo pacman -S qtcurve
+#这个似乎不全,好像缺了配置gui
 sudo pacman -S fcitx-im
-sudo pacman -S xorg
 sudo pacman -S base
 sudo pacman -S base-devel
 sudo pacman -S qt
+#xorg太大了,可以挑选下
+sudo pacman -S xorg
+#lang包可以去除中文以外的
 sudo pacman -S texlive-lang
 sudo pacman -S texlive-most
 #sudo pacman -S vim-plugins
