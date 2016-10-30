003c231d7ad688c2c291ba775a6c6174ca90eda4
rbn42 <bl100@students.waikato.ac.nz>
2016-10-31 12:38:34 +1300

    groups.sh

---
+++ groups.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#有用的不多
+#大部分是DE的group
+sudo pacman -S qtcurve
+sudo pacman -S fcitx-im
+sudo pacman -S xorg
+sudo pacman -S base
+sudo pacman -S base-devel
+sudo pacman -S qt
+sudo pacman -S texlive-lang
+sudo pacman -S texlive-most
+#sudo pacman -S vim-plugins
