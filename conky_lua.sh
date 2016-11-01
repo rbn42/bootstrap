c95b58e74d45a0f303391b82399c5cdb7b48116b
rbn42 <bl100@students.waikato.ac.nz>
2016-11-01 15:30:06 +1300

    conky_lua.sh

--- conky_lua.sh
+++ conky_lua.sh
@@ -1,12 +1,16 @@
-#conky lua 的安装
+#2016-11-01 15:30:01 Tue NZDT
+#aur conky lua 的安装
+
 #因为lua版本问题, cmake没法给conky找到lua路径,所以需要如下的配置
+
 cd /dev/shm
 git clone https://aur.archlinux.org/conky-git.git
-#修改build,加入这些配置
+cd conky-git
 
+#修改build(),加入这些配置
 #    -D BUILD_LUA_CAIRO=ON \
 #    -D APP_TOLUA=/usr/bin/toluapp \
 #    -D TOLUA_INCLUDE_PATH=/usr/include \
 #    -D TOLUA_LIBS=/usr/lib/libtoluapp.so \
-cd conky-git
+
 makepkg -i
